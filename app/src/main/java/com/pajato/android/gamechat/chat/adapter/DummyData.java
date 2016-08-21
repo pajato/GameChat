e6541457bd490cde27fccc920fa03ada8ca6578a
Paul Michael Reilly <pmr@pajato.com>
2016-08-21 14:23:17 -0400

    Apply review comments, plus...
    <h1>Rationale:</h1>
    
    Apply suggestions from Bryan and Sandy; remove warnings; and fix a broken test.
    
    <h1>File changes:</h1>
    
    modified:   app/src/main/java/com/pajato/android/gamechat/chat/RoomsFragment.java
    
    - Fix a broken test by ensuring that removing listeners is only done using a non-null listener.
    
    modified:   app/src/main/java/com/pajato/android/gamechat/chat/adapter/DummyData.java
    
    - Use a better set of data headers (coarser granularity is better).
    
    modified:   app/src/main/java/com/pajato/android/gamechat/chat/adapter/GroupListItem.java
    
    - Add bolding of rooms with new messages.
    
    modified:   app/src/main/java/com/pajato/android/gamechat/chat/adapter/RoomsListAdapter.java
    
    - Support the use of HTML to allow for bolded rooms.
    
    modified:   app/src/main/res/layout/fragment_rooms.xml
    
    - Make the recycler view use a bottom margin so as not to overwrite the paging markers.
    
    modified:   app/src/main/res/layout/item_date_header.xml
    modified:   app/src/main/res/values/styles.xml
    
    - Use a style on the date header to avoid and overdraw warning.
    - Fix some RTL warnings.
    
    modified:   app/src/main/res/layout/item_group_list.xml
    modified:   app/src/main/res/values/strings_room_pane.xml
    
    - Fix a content description warning.

--- app/src/main/java/com/pajato/android/gamechat/chat/adapter/DummyData.java
+++ app/src/main/java/com/pajato/android/gamechat/chat/adapter/DummyData.java
@@ -3,9 +3,9 @@ package com.pajato.android.gamechat.chat.adapter;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.pajato.android.gamechat.chat.adapter.DateHeaderItem.DateHeaderType.now;
-import static com.pajato.android.gamechat.chat.adapter.DateHeaderItem.DateHeaderType.old;
 import static com.pajato.android.gamechat.chat.adapter.DateHeaderItem.DateHeaderType.thisMonth;
+import static com.pajato.android.gamechat.chat.adapter.DateHeaderItem.DateHeaderType.today;
+import static com.pajato.android.gamechat.chat.adapter.DateHeaderItem.DateHeaderType.yesterday;
 
 /**
  * Provide a POJO with some dummy data to use while developing the rooms screen.
@@ -16,15 +16,15 @@ public class DummyData {
 
     public static List<RoomsListItem> getData() {
         List<RoomsListItem> list = new ArrayList<>();
-        list.add(new RoomsListItem(new DateHeaderItem(now)));
+        list.add(new RoomsListItem(new DateHeaderItem(today)));
         list.add(new RoomsListItem(new GroupListItem("0")));
         list.add(new RoomsListItem(new GroupListItem("1")));
-        list.add(new RoomsListItem(new DateHeaderItem(thisMonth)));
+        list.add(new RoomsListItem(new DateHeaderItem(yesterday)));
         list.add(new RoomsListItem(new GroupListItem("2")));
         list.add(new RoomsListItem(new GroupListItem("3")));
         list.add(new RoomsListItem(new GroupListItem("4")));
         list.add(new RoomsListItem(new GroupListItem("5")));
-        list.add(new RoomsListItem(new DateHeaderItem(old)));
+        list.add(new RoomsListItem(new DateHeaderItem(thisMonth)));
         list.add(new RoomsListItem(new GroupListItem("6")));
         list.add(new RoomsListItem(new GroupListItem("7")));
 
